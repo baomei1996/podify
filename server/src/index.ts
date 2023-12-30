86ae88a35e6ba5064db43256a01324c9474ffda8
baomei1996 <qhal4686@gmail.com>
2023-12-30 11:27:24 +0900

    Feat: Add server plan.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "dotenv/config";
+import "./db";
 
 const app = express();
 
@@ -7,3 +9,14 @@ const PORT = 8989;
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
+/**
+ * The plan and features
+ * upload audio files
+ * listen to single audio
+ * add to favorites
+ * create playlist
+ * remove playlist (public/private)
+ * remove audios
+ * many more...
+ */
