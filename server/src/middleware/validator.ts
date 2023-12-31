69b045d9512573bcc7ebcaa4281840edd7d5419f
baomei1996 <qhal4686@gmail.com>
2023-12-31 16:15:59 +0900

    Feat: Add create user validator.

---
+++ server/src/middleware/validator.ts
@@ -0,0 +1,32 @@
+import { RequestHandler } from "express";
+import * as yup from "yup";
+
+export const validate = (schema: any): RequestHandler => {
+    return async (req, res, next) => {
+        if (!req.body) {
+            return res.json({
+                error: "Empty body is not expected!",
+            });
+        }
+
+        try {
+            const schemaToValidate = yup.object({
+                body: schema,
+            });
+            await schemaToValidate.validate(
+                {
+                    body: req.body,
+                },
+                {
+                    abortEarly: true,
+                }
+            );
+
+            next();
+        } catch (error) {
+            if (error instanceof yup.ValidationError) {
+                return res.json({ error: error.message });
+            }
+        }
+    };
+};
---
+++ server/src/utils/validationSchema.ts
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const CreateUserSchema = yup.object().shape({
+    name: yup
+        .string()
+        .trim()
+        .required("Name is missing!")
+        .min(3, "Name is too short!")
+        .max(20, "Name is too long!"),
+    email: yup
+        .string()
+        .required("Email is missing!")
+        .email("Email is invalid!"),
+    password: yup
+        .string()
+        .trim()
+        .required("Password is missing!")
+        .min(8, "Password is too short!")
+        .matches(
+            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+            "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character!"
+        ),
+});
