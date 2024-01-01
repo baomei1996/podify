807f36369be23de9779cc80aa6dc5e065e13e6ea
baomei1996 <qhal4686@gmail.com>
2024-01-01 17:52:08 +0900

    Feat: Sending first email

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,10 +1,29 @@
 import { RequestHandler } from "express";
+import nodemailer from "nodemailer";
 
 import { CreateUser } from "@/@types/user";
 import User from "@/models/user";
+import { MAILTRAP_PASS, MAILTRAP_USER } from "@/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
     const { email, password, name } = req.body;
     const user = await User.create({ email, password, name });
+
+    // send verification email
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: MAILTRAP_USER,
+            pass: MAILTRAP_PASS,
+        },
+    });
+
+    transport.sendMail({
+        to: user.email,
+        from: "auth@myapp.com",
+        html: `<h1>Hello ${user.name}</h1>`,
+    });
+
     res.status(201).json({ user });
 };
