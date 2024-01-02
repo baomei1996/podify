721a39f1c2a687a9238499482f70fd4152c32de6
baomei1996 <qhal4686@gmail.com>
2024-01-02 23:36:03 +0900

    Feat: Verifying Emails

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express";
 
-import { CreateUser } from "@/@types/user";
+import { CreateUser, VerifyEmailRequest } from "@/@types/user";
 import User from "@/models/user";
 import { generateToken } from "@/utils/helper";
 import { sendVerificationMail } from "@/utils/mail";
+import EmailVerificationToken from "@/models/emailVerificationToken";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
     const { email, password, name } = req.body;
@@ -13,4 +14,30 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     const token = generateToken();
     sendVerificationMail(token, { name, email, userId: user._id.toString() });
     res.status(201).json({ user });
+};
+
+export const verifyEmail: RequestHandler = async (
+    req: VerifyEmailRequest,
+    res
+) => {
+    const { token, userId } = req.body;
+    const verificationToken = await EmailVerificationToken.findOne({
+        owner: userId,
+    });
+
+    if (!verificationToken) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
+
+    const matched = await verificationToken.compareToken(token);
+    if (!matched) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
+
+    // 정상적인 토큰일 경우 verified를 true로 업데이트
+    await User.findByIdAndUpdate(userId, { verified: true });
+    // 토큰 삭제
+    await EmailVerificationToken.findByIdAndDelete(verificationToken._id);
+
+    res.status(200).json({ message: "your email is verified" });
 };
--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { validate } from "@/middleware/validator";
 import { CreateUserSchema } from "@/utils/validationSchema";
-import { create } from "@/controllers/user";
+import { create, verifyEmail } from "@/controllers/user";
 
 const router = Router();
 
 // Create a new user
 router.post("/create", validate(CreateUserSchema), create);
+router.post("/verify-email", verifyEmail);
 
 export default router;
