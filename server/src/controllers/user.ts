9ecb6033ed73892c6f5115743c53fc5667e2b70a
baomei1996 <qhal4686@gmail.com>
2024-01-07 19:31:36 +0900

    Feat: Verifying password reset token

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -106,3 +106,23 @@ export const generateForgetPasswordLink: RequestHandler = async (req, res) => {
 
     res.json({ message: "Check your register mail." });
 };
+
+export const isValidPassResetToken: RequestHandler = async (req, res) => {
+    const { token, userId } = req.body;
+
+    const resetToken = await passwordResetToken.findOne({ owner: userId });
+    if (!resetToken) {
+        return res
+            .status(403)
+            .json({ message: "Unauthorized access, invalid token!" });
+    }
+
+    const matched = await resetToken?.compareToken(token);
+    if (!matched) {
+        return res
+            .status(403)
+            .json({ message: "Unauthorized access, invalid token!" });
+    }
+
+    res.json({ message: "your token is valid!" });
+};
--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -3,21 +3,27 @@ import { Router } from "express";
 import { validate } from "@/middleware/validator";
 import {
     CreateUserSchema,
-    EmailVerificationBody,
+    TokenAndIDValidation,
 } from "@/utils/validationSchema";
 import {
     create,
     verifyEmail,
     sendReVerificationToken,
     generateForgetPasswordLink,
+    isValidPassResetToken,
 } from "@/controllers/user";
 
 const router = Router();
 
 // Create a new user
 router.post("/create", validate(CreateUserSchema), create);
-router.post("/verify-email", validate(EmailVerificationBody), verifyEmail);
+router.post("/verify-email", validate(TokenAndIDValidation), verifyEmail);
 router.post("/re-verify-email", sendReVerificationToken);
 router.post("/forget-password", generateForgetPasswordLink);
+router.post(
+    "/verify-pass-reset-token",
+    validate(TokenAndIDValidation),
+    isValidPassResetToken
+);
 
 export default router;
